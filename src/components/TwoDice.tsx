40d3828dc9cdb91c091bedf0527322bb509467b1
Leah Marcelli <leahim@udel.edu>
2024-09-25 19:44:12 -0400

    updated TwoDice

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -7,43 +7,37 @@ import { Button } from "react-bootstrap";
  * module (which returns a random decimal between 0 up until 1) in order
  * to produce a random integer between 1 and 6 (inclusive).
  */
+
 export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
 export function TwoDice(): React.JSX.Element {
-    const [leftDice, setLeftDice] = useState<number>(d6());
-    const [rightDice, setRightDice] = useState<number>(d6());
-    const [gameStatus, setGameStatus] = useState<string | null>(null);
+    const [leftDie, setLeftDie] = useState<number>(1);
+    const [rightDie, setRightDie] = useState<number>(2);
 
-    const checkGameStatus = (left: number, right: number): void => {
-        if (left === right) {
-            setGameStatus(left === 1 ? "Lose" : "Win");
-        } else {
-            setGameStatus(null); // Reset status if not matched
-        }
-    };
-    const leftRoll = (): void => {
-        const newValue = d6();
-        setLeftDice(newValue);
-        checkGameStatus(newValue, rightDice);
+    const rollLeft = () => {
+        setLeftDie(d6());
     };
 
-    const rightRoll = (): void => {
-        const newValue = d6();
-        setRightDice(newValue);
-        checkGameStatus(leftDice, newValue);
+    const rollRight = () => {
+        setRightDie(d6());
     };
+
+    // const isWin = leftDie === rightDie;
+    // const isLose = leftDie === 1 && rightDie === 1;
 
     return (
         <div>
+            <span data-testid="left-die">{leftDie}</span>
+            <span data-testid="right-die">{rightDie}</span>
             <div>
-                <span data-testid="left-die">{leftDice}</span>
-                <span data-testid="right-die">{rightDice}</span>
+                <Button onClick={rollLeft}>Roll Left</Button>
+                <Button onClick={rollRight}>Roll Right</Button>
             </div>
-            <Button onClick={leftRoll}>Roll Left</Button>
-            <Button onClick={rightRoll}>Roll Right</Button>
-            {gameStatus && <div>{gameStatus}</div>}
+            {leftDie === rightDie && (
+                <p>{leftDie === 1 ? "You lose" : "You Win"}</p>
+            )}
         </div>
     );
 }
